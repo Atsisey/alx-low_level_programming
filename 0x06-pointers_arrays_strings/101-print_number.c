da9b1974020d08061b29cd2992169c894bcf09dd
Atsisey <ablaatsisey77@gmail.com>
2022-12-22 15:37:17 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,21 @@
 #include "main.h"
-
 /**
- * print_number - print numbers chars
- * @n: integer parameters
- * Return: 0
- */
-
+* print_number - print numbers chars
+* @n: integer parameters
+*
+*/
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int num = n;
 
-	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		num = -num;
 	}
-	if (n1 / 10 != 0)
-	{
-		print_number(m1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
