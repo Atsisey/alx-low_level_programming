82759c399ed14be11ba1332ffdbe1ca1b024a21c
Atsisey <ablaatsisey77@gmail.com>
2023-01-03 10:53:52 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -8,7 +8,7 @@
 int main(void)
 {
 	char c;
-	
+
 	char d;
 
 	c = 'a';
@@ -26,4 +26,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-			
